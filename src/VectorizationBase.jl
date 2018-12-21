3efd3a002905d46932038949738dbda3ae4fb8e7
Chris Elrod <elrodc@gmail.com>
2018-12-21 07:14:03 -0600

    Added more SVec constructors.

--- src/VectorizationBase.jl
+++ src/VectorizationBase.jl
@@ -18,17 +18,31 @@ const Vec{N,T} = NTuple{N,VE{T}}
 abstract type AbstractStructVec{N,T} end
 struct SVec{N,T} <: AbstractStructVec{N,T}
     data::Vec{N,T}
+    SVec{N,T}(v) where {N,T} = new(v)
 end
-SVec{N,T}(x::T) where {N,T} = SVec(ntuple(i -> VE(x), Val(N)))
+# SVec{N,T}(x) where {N,T} = SVec(ntuple(i -> VE(T(x)), Val(N)))
+function SVec{N,T}(x::Number) where {N,T}
+    SVec(ntuple(i -> VE(T(x)), Val(N)))
+end
+function SVec(v::Vec{N,T}) where {N,T}
+    SVec{N,T}(v)
+end
 @inline Base.length(::AbstractStructVec{N}) where N = N
 @inline Base.size(::AbstractStructVec{N}) where N = (N,)
 @inline Base.eltype(::AbstractStructVec{N,T}) where {N,T} = T
 @inline conj(v::AbstractStructVec) = v # so that things like dot products work.
 @inline adjoint(v::AbstractStructVec) = v # so that things like dot products work.
 
+function Base.one(::Type{<:AbstractStructVec{N,T}}) where {N,T}
+    SVec{N,T}(one(T))
+end
+function Base.one(::AbstractStructVec{N,T}) where {N,T}
+    SVec{N,T}(one(T))
+end
+
 const AbstractSIMDVector{N,T} = Union{Vec{N,T},AbstractStructVec{N,T}}
 
-@inline extract_data(v::Vec) = v
+@inline extract_data(v) = v
 @inline extract_data(v::SVec) = v.data
 
 """
