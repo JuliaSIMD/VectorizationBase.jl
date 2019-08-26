8b69916af2f5a770ae7c21928b3f7d69714d05c7
Chris Elrod <elrodc@gmail.com>
2019-08-26 06:39:28 -0500

    Added another align method.

--- src/allignment.jl
+++ src/allignment.jl
@@ -1,8 +1,5 @@
 
-
-
-
 align(x) = (x + REGISTER_SIZE-1) & -REGISTER_SIZE
 align(x::Ptr{T}) where {T} = reinterpret(Ptr{T}, align(reinterpret(UInt, x)))
 
-
+align(x, n) = (nm1 = n - 1; (x + nm1) & ~nm1)
