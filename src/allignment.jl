3c4c39da2d786aa6438142b87524bd342767fd14
Chris Elrod <elrodc@gmail.com>
2019-09-05 07:46:16 -0500

    Added allignment to type-size method.

--- src/allignment.jl
+++ src/allignment.jl
@@ -1,5 +1,5 @@
 
 align(x) = (x + REGISTER_SIZE-1) & -REGISTER_SIZE
 align(x::Ptr{T}) where {T} = reinterpret(Ptr{T}, align(reinterpret(UInt, x)))
-
 align(x, n) = (nm1 = n - 1; (x + nm1) & ~nm1)
+align(x, ::Type{T}) where {T} = align(x, sizeof(T))
