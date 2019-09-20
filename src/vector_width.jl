2384e362c6750e35df287050184069e4da8be438
Chris Elrod <elrodc@gmail.com>
2019-09-20 06:58:20 -0500

    ::Type{T} -> T in many method signatures.

--- src/vector_width.jl
+++ src/vector_width.jl
@@ -6,23 +6,23 @@ function intlog2(N)
     # u += N
     u = 0x4330000000000000 + N
     d = reinterpret(Float64,u) - 4503599627370496.0
-    u = (reinterpret(Int64, d) >> 52) - 1023
+    (reinterpret(Int64, d) >> 52) - 1023
 end
 
-@generated function T_shift(::Type{T}) where {T}
+@generated function T_shift(T)
     intlog2(sizeof(T))
 end
-@generated function pick_vector_width(::Type{T} = Float64) where T
+@generated function pick_vector_width(T = Float64)
     shift = T_shift(T)
     max(1, REGISTER_SIZE >> shift)
 end
-@generated function pick_vector_width_shift(::Type{T} = Float64) where T
+@generated function pick_vector_width_shift(T = Float64)
     shift = T_shift(T)
     W = max(1, REGISTER_SIZE >> shift)
     Wshift = intlog2(W)
     W, Wshift
 end
-function pick_vector_width(N::Integer, ::Type{T} = Float64) where T
+function pick_vector_width(N::Integer, T = Float64)
     W = pick_vector_width(T)
     TwoN = 2N
     while W >= TwoN
@@ -30,7 +30,7 @@ function pick_vector_width(N::Integer, ::Type{T} = Float64) where T
     end
     W
 end
-function pick_vector_width_shift(N::Integer, ::Type{T} = Float64) where T
+function pick_vector_width_shift(N::Integer, T = Float64)
     W, Wshift = pick_vector_width_shift(T)
     TwoN = 2N
     while W >= TwoN
@@ -39,7 +39,7 @@ function pick_vector_width_shift(N::Integer, ::Type{T} = Float64) where T
     end
     W, Wshift
 end
-pick_vector_width(::Symbol, T::DataType) = pick_vector_width(T)
-pick_vector_width_shift(::Symbol, T::DataType) = pick_vector_width_shift(T)
+pick_vector_width(::Symbol, T) = pick_vector_width(T)
+pick_vector_width_shift(::Symbol, T) = pick_vector_width_shift(T)
 
 @generated pick_vector_width(::Val{N}, ::Type{T} = Float64) where {N,T} = pick_vector_width(N, T)
