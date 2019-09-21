169f9028b88b18051f469946ca3ed0af11f4e2bc
Chris Elrod <elrodc@gmail.com>
2019-09-21 12:08:38 -0500

    Added truncating allignment function.

--- src/alignment.jl
+++ src/alignment.jl
@@ -1,5 +1,10 @@
 
 align(x) = (x + REGISTER_SIZE-1) & -REGISTER_SIZE
 align(x::Ptr{T}) where {T} = reinterpret(Ptr{T}, align(reinterpret(UInt, x)))
-align(x, n) = (nm1 = n - 1; (x + nm1) & ~nm1)
+align(x, n) = (nm1 = n - 1; (x + nm1) & -n)
 align(x, ::Type{T}) where {T} = align(x, REGISTER_SIZE ÷ sizeof(T))
+aligntrunc(x, n) = x & -n
+aligntrunc(x) = aligntrunc(x, REGISTER_SIZE)
+aligntrunc(x, ::Type{T}) where {T} = x > pick_vector_width(T) ? aligntrunc(x, REGISTER_SIZE ÷ sizeof(T)) : x
+
+
