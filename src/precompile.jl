b2c1b05bf8578788a9e958ec297bd3836ad2b747
Chris Elrod <elrodc@gmail.com>
2019-12-12 08:27:46 -0500

    Updated precompile.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,4 +1,6 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
+    precompile(Tuple{typeof(VectorizationBase.T_shift),Type{Float64}})
+    precompile(Tuple{typeof(VectorizationBase.llvmtype),TypeVar})
     precompile(Tuple{typeof(VectorizationBase.mask_type),Type{Float64}})
 end
