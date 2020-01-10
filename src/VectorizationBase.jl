661325e6a10f08caa83a856e2959c1d6fd6dc872
chriselrod <elrodc@gmail.com>
2020-01-09 23:27:56 -0500

    Moved vbroadcast here from SIMDPirates.

--- src/VectorizationBase.jl
+++ src/VectorizationBase.jl
@@ -9,7 +9,7 @@ export Vec, VE, SVec,
     pick_vector_width_shift,
     vectorizable, stridedpointer,
     Pointer, ZeroInitializedPointer,
-    load, store!
+    load, store!, vbroadcast
 
 const VE{T} = Core.VecElement{T}
 const Vec{N,T} = NTuple{N,VE{T}}
@@ -29,6 +29,38 @@ end
 # @inline function SVec(v::Vec{N,T}) where {N,T}
     # SVec{N,T}(v)
 # end
+@generated function vbroadcast(::Type{Vec{W,T}}, s::T) where {W, T <: ScalarTypes}
+    typ = llvmtype(T)
+    vtyp = vtyp1 = "<$W x $typ>"
+    instrs = String[]
+    push!(instrs, "%ie = insertelement $vtyp undef, $typ %0, i32 0")
+    push!(instrs, "%v = shufflevector $vtyp %ie, $vtyp undef, <$W x i32> zeroinitializer")
+    push!(instrs, "ret $vtyp %v")
+    quote
+        $(Expr(:meta,:inline))
+        Base.llvmcall( $(join(instrs,"\n")), Vec{$W,$T}, Tuple{$T}, s )
+    end
+end
+@inline vbroadcast(::Val{W}, s::T) where {W,T} = SVec(vbroadcast(Vec{W,T}, s))
+@inline vbroadcast(::Val{W}, ptr::Ptr{T}) where {W,T} = SVec(vbroadcast(Vec{W,T}, VectorizationBase.load(ptr)))
+@inline vbroadcast(::Type{Vec{W,T1}}, s::T2) where {W,T1,T2} = vbroadcast(Vec{W,T1}, convert(T1,s))
+@inline vbroadcast(::Type{Vec{W,T}}, ptr::Ptr{T}) where {W,T} = vbroadcast(Vec{W,T}, VectorizationBase.load(ptr))
+@inline vbroadcast(::Type{Vec{W,T}}, ptr::Ptr) where {W,T} = vbroadcast(Vec{W,T}, Base.unsafe_convert(Ptr{T},ptr))
+@inline vbroadcast(::Type{SVec{W,T}}, s) where {W,T} = SVec(vbroadcast(Vec{W,T}, s))
+@inline vbroadcast(::Type{Vec{W,T}}, v::Vec{W,T}) where {W,T} = v
+@inline vbroadcast(::Type{SVec{W,T}}, v::SVec{W,T}) where {W,T} = v
+@inline vbroadcast(::Type{SVec{W,T}}, v::Vec{W,T}) where {W,T} = SVec(v)
+
+@inline vone(::Type{Vec{N,T}}) where {N,T} = vbroadcast(Vec{W,T}, one(T))
+@inline vzero(::Type{Vec{N,T}}) where {N,T} = vbroadcast(Vec{W,T}, zero(T))
+@inline vone(::Type{SVec{N,T}}) where {N,T} = SVec(vbroadcast(Vec{W,T}, one(T)))
+@inline vzero(::Type{SVec{N,T}}) where {N,T} = SVec(vbroadcast(Vec{W,T}, zero(T)))
+@inline vone(::Type{T}) where {T} = one(T)
+@inline vzero(::Type{T}) where {T} = zero(T)
+@inline VectorizationBase.SVec{W,T}(s::T) where {W,T} = SVec(vbroadcast(Vec{W,T}, s))
+@inline VectorizationBase.SVec{W,T}(s::Number) where {W,T} = SVec(vbroadcast(Vec{W,T}, convert(T, s)))
+
+
 @inline SVec(v::SVec) = v
 @inline Base.length(::AbstractStructVec{N}) where N = N
 @inline Base.size(::AbstractStructVec{N}) where N = (N,)
@@ -42,10 +74,10 @@ end
     # @inbounds SVec(ntuple(n -> Core.VecElement{T}(T(v[n])), Val(N)))
 # end
 
-@inline Base.one(::Type{<:AbstractStructVec{N,T}}) where {N,T} = SVec{N,T}(one(T))
-@inline Base.one(::AbstractStructVec{N,T}) where {N,T} = SVec{N,T}(one(T))
-@inline Base.zero(::Type{<:AbstractStructVec{N,T}}) where {N,T} = SVec{N,T}(zero(T))
-@inline Base.zero(::AbstractStructVec{N,T}) where {N,T} = SVec{N,T}(zero(T))
+@inline Base.one(::Type{<:AbstractStructVec{W,T}}) where {W,T} = SVec(vbroadcast(Vec{W,T}, one(T)))
+@inline Base.one(::AbstractStructVec{W,T}) where {W,T} = SVec(vbroadcast(Vec{W,T}, one(T)))
+@inline Base.zero(::Type{<:AbstractStructVec{W,T}}) where {W,T} = SVec(vbroadcast(Vec{W,T}, zero(T)))
+@inline Base.zero(::AbstractStructVec{W,T}) where {W,T} = SVec(vbroadcast(Vec{W,T}, zero(T)))
 
 
 
