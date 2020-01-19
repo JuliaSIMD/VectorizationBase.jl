06ba1075bc1ab8bc60aa6f3a8d1fc878e76a989e
Chris Elrod <elrodc@gmail.com>
2020-01-18 22:56:50 -0500

    Modified static methods meant to enable taking advantage of size info known at compile time.

--- src/static.jl
+++ src/static.jl
@@ -2,12 +2,12 @@
 struct Static{N} end
 Base.@pure Static(N) = Static{N}()
 
-struct StaticUnitRange{L,U} end
+struct StaticUnitRange{L,U} <: AbstractRange{Int} end
 Base.@pure StaticUnitRange(L,U) = StaticUnitRange{L,U}()
-struct StaticLowerUnitRange{L}
+struct StaticLowerUnitRange{L} <: AbstractRange{Int}
     U::Int
 end
-struct StaticUpperUnitRange{U}
+struct StaticUpperUnitRange{U} <: AbstractRange{Int}
     L::Int
 end
 
@@ -16,10 +16,10 @@ end
 @inline Base.:(:)(::Static{L}, U::Int) where {L} = StaticLowerUnitRange{L}(U)
 @inline Base.:(:)(L::Int, ::Static{U}) where {U} = StaticUpperUnitRange{U}(L)
 
-@inline maybestaticsize(A, args...) = size(A, args...)
+@inline maybestaticsize(A, ::Val{I}) where {I} = size(A, I)
 @inline maybestaticlength(A) = length(A)
 
-@inline maybestaticsize(::NTuple{N}, i) where {N} = Static{N}() # should we assert that i == 1?
+@inline maybestaticsize(::NTuple{N}, ::Val{1}) where {N} = Static{N}() # should we assert that i == 1?
 @inline maybestaticlength(::NTuple{N}) where {N} = Static{N}()
 
 
@@ -45,3 +45,5 @@ end
 @inline Base.:&(i, ::Static{N}) where {N} = N & i
 @inline Base.:&(::Static{M}, ::Static{N}) where {M,N} = M & N
 
+@generated staticm1(::Static{N}) where {N} = Static{N-1}()
+@inline staticm1(N::Integer) = N - 1
