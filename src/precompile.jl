9f70f4bc64d431c208abe51f824fa170f864925c
Chris Elrod <elrodc@gmail.com>
2020-01-22 12:00:35 -0500

    Bump version.

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,7 +1,6 @@
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
-    precompile(Tuple{typeof(VectorizationBase.T_shift),Type{Float64}})
-    precompile(Tuple{typeof(VectorizationBase.llvmtype),TypeVar})
-    precompile(Tuple{typeof(VectorizationBase.llvmtype),Type{T} where T})
-    precompile(Tuple{typeof(VectorizationBase.mask_type),Type{Float64}})
+    precompile(Tuple{typeof(VectorizationBase.mask),Val{8},Int64})
+    precompile(Tuple{typeof(VectorizationBase.pick_vector_width),Type{Float32}})
+    precompile(Tuple{typeof(VectorizationBase.unstable_mask),Int64,Int64})
 end
