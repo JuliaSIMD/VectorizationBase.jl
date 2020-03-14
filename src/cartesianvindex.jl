d19fc05af8f28b741f1f9630a6c6733a4b5aaa08
chriselrod <elrodc@gmail.com>
2020-03-14 17:30:12 -0400

    Tuple was not defined for CartesianVIndex.

--- src/cartesianvindex.jl
+++ src/cartesianvindex.jl
@@ -5,7 +5,7 @@ struct CartesianVIndex{N,T} <: Base.AbstractCartesianIndex{N}
 end
 Base.length(::CartesianVIndex{N}) where {N} = N
 Base.length(::Type{<:CartesianVIndex{N}}) where {N} = N
-
+Base.Tuple(i::CartesianVIndex) = i.I
 function Base.:(:)(I::CartesianVIndex{N}, J::CartesianVIndex{N}) where {N}
-    CartesianIndices(map((i,j) -> i:j, Tuple(I), Tuple(J)))
+    CartesianIndices(map((i,j) -> i:j, I.I, J.I))
 end
