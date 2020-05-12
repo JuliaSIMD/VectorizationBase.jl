45f22355e0e50d10c8020f04c700240c931008bb
chriselrod <elrodc@gmail.com>
2020-05-12 11:40:18 -0400

    Trying random things to see if it eases inference.

--- src/VectorizationBase.jl
+++ src/VectorizationBase.jl
@@ -117,13 +117,18 @@ end
 end
 @generated function vzero(::Type{Vec{W,T}}) where {W,T}
     typ = llvmtype(T)
-    vtyp = "<$W x $typ>"
-    instrs = """
-    ret $vtyp zeroinitializer
-    """
+    instrs = "ret <$W x $typ> zeroinitializer"
     quote
         $(Expr(:meta,:inline))
         Base.llvmcall($instrs, Vec{$W,$T}, Tuple{}, )
+    end
+end
+@generated function vzero(::Val{W}, ::Type{T}) where {W,T}
+    typ = llvmtype(T)
+    instrs = "ret <$W x $typ> zeroinitializer"
+    quote
+        $(Expr(:meta,:inline))
+        SVec(Base.llvmcall($instrs, Vec{$W,$T}, Tuple{}, ))
     end
 end
 # @inline vzero(::Type{Vec{W,T}}) where {W,T} = vzero(Val{W}(), T)
@@ -140,7 +145,6 @@ end
 @inline vone(::Type{Vec{W,T}}) where {W,T} = vbroadcast(Vec{W,T}, one(T))
 # @inline vzero(::Type{Vec{W,T}}) where {W,T} = vbroadcast(Vec{W,T}, zero(T))
 @inline vone(::Type{SVec{W,T}}) where {W,T} = SVec(vbroadcast(Vec{W,T}, one(T)))
-@inline vzero(::Val{W}, ::Type{T}) where {W,T} = SVec(vzero(Vec{W,T}))
 @inline vzero(::Type{SVec{W,T}}) where {W,T} = SVec(vzero(Vec{W,T}))
 @inline vone(::Type{T}) where {T} = one(T)
 @inline vzero(::Type{T}) where {T<:Number} = zero(T)
