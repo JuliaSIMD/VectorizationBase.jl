4e9a618538aaffca1a62e873e815934b264381a1
chriselrod <elrodc@gmail.com>
2020-12-02 16:58:16 -0500

    Handle 1-4 possible cache levels.

--- src/topology.jl
+++ src/topology.jl
@@ -5,45 +5,58 @@ const CACHE = TOPOLOGY.children[1].children[1];
 const COUNTS = Hwloc.histmap(TOPOLOGY);
 # TODO: Makes topological assumptions that aren't right for
 # multiple nodes or with >3 or <3 levels of Cache.
-const L₁CACHE = (
-    size = CACHE.children[1].children[1].attr.size,
-    depth = CACHE.children[1].children[1].attr.depth,
-    linesize = CACHE.children[1].children[1].attr.linesize,
-    associativity = CACHE.children[1].children[1].attr.associativity,
-    type = CACHE.children[1].children[1].attr.type_
+
+const CACHE_COUNT = (
+    COUNTS[:L1Cache],
+    COUNTS[:L2Cache],
+    COUNTS[:L3Cache],
+    COUNTS[:L4Cache]
 )
-const L₂CACHE = (
-    size = CACHE.children[1].attr.size,
-    depth = CACHE.children[1].attr.depth,
-    linesize = CACHE.children[1].attr.linesize,
-    associativity = CACHE.children[1].attr.associativity,
-    type = CACHE.children[1].attr.type_
-)
-const L₃CACHE = (
-    size = CACHE.attr.size,
-    depth = CACHE.attr.depth,
-    linesize = CACHE.attr.linesize,
-    associativity = CACHE.attr.associativity,
-    type = CACHE.attr.type_
-)
+const NUM_CORES = COUNTS[:Core]
+
+const CACHE_LEVELS = something(findfirst(isequal(0), CACHE_COUNT) - 1, length(CACHE_COUNT) + 1)
+
+function define_cache(N)
+    if N > CACHE_LEVELS
+        return (
+            size = nothing,
+            depth = nothing,
+            linesize = nothing,
+            associativity = nothing,
+            type = nothing
+        )
+    end
+    c = CACHE
+    for n ∈ 1:CACHE_LEVELS-N
+        c = first(c.children)
+    end
+    (
+        size = c.attr.size,
+        depth = c.attr.depth,
+        linesize = c.attr.linesize,
+        associativity = c.attr.associativity,
+        type = c.attr.type_
+    )
+end
+
+
+const L₁CACHE = define_cache(1)
+const L₂CACHE = define_cache(2)
+const L₃CACHE = define_cache(3)
+const L₄CACHE = define_cache(4)
 """
 L₁, L₂, L₃ cache size
 """
 const CACHE_SIZE = (
     L₁CACHE.size,
     L₂CACHE.size,
-    L₃CACHE.size
+    L₃CACHE.size,
+    L₄CACHE.size
 )
 # const CACHE_NEST_COUNT = (
 #     length(CACHE.children[1].children),
 #     length(CACHE.children),
 #     length(TOPOLOGY.children[1].children)
 # )
-const CACHE_COUNT = (
-    COUNTS[:L1Cache],
-    COUNTS[:L2Cache],
-    COUNTS[:L3Cache]
-)
-const NUM_CORES = COUNTS[:Core]
 
 
