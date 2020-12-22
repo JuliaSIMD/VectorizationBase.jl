870f87f73986c1f30f1e2d740cd6821d2e7ef82e
Michael Abbott <me@escbook>
2020-12-22 23:55:48 +0100

    fix relu

--- src/special/misc.jl
+++ src/special/misc.jl
@@ -1,6 +1,6 @@
 @inline Base.:^(v::AbstractSIMD{W,T}, i::Integer) where {W,T} = Base.power_by_squaring(v, i)
 @inline Base.:^(v::AbstractSIMD{W,T}, i::Integer) where {W,T<:Union{Float32,Float64}} = Base.power_by_squaring(v, i)
-@inline relu(x) = (y = zero(x); IfElse.ifelse(x > y, x, y))
+@inline relu(x) = (y = zero(x); ifelse(x > y, x, y))
 
 @inline Base.fld(x::AbstractSIMD, y::AbstractSIMD) = div(promote(x,y)..., RoundDown)
 
