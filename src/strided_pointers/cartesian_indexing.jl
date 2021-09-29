b6f8191282950bfbcf1dd1fcbcd042e09e90f201
chriselrod <elrodc@gmail.com>
2021-09-29 02:18:35 -0500

    handle fewer stride multiples, e.g. if dims lowers

--- src/strided_pointers/cartesian_indexing.jl
+++ src/strided_pointers/cartesian_indexing.jl
@@ -4,20 +4,26 @@
 @inline tdot(ptr::AbstractStridedPointer{T}, a, b) where {T} = tdot(pointer(ptr), a, b)
 
 @inline function tdot(p::Ptr{T}, a::Tuple{A}, b::Tuple{B,Vararg}) where {T,A,B}
-    p, lazymul(first(a), first(b))
+  p, lazymul(first(a), first(b))
 end
 @inline function tdot(p::Ptr{T}, a::Tuple{A}, b::Tuple{B,Vararg}, c::Tuple{C,Vararg}) where {T,A,B,C}
-    p, lazymul(first(a), first(b), first(c))
+  p, lazymul(first(a), first(b), first(c))
 end
 
 @inline function tdot(p::Ptr{T}, a::Tuple{A1,A2,Vararg}, b::Tuple{B1,B2,Vararg}) where {T,A1,A2,B1,B2}
-    i = lazymul(  first(a), first(b))
-    p, j = tdot(p, tail(a),  tail(b))
-    add_indices(p, i, j)
+  i = lazymul(  first(a), first(b))
+  p, j = tdot(p, tail(a),  tail(b))
+  add_indices(p, i, j)
 end
 @inline function tdot(p::Ptr{T}, a::Tuple{A1,A2,Vararg}, b::Tuple{B1,B2,Vararg}, c::Tuple{C1,C2,Vararg}) where {T,A1,A2,B1,B2,C1,C2}
-    i = lazymul(  first(a), first(b), first(c))
-    p, j = tdot(p, tail(a),  tail(b),  tail(c))
-    add_indices(p, i, j)
+  i = lazymul(  first(a), first(b), first(c))
+  p, j = tdot(p, tail(a),  tail(b),  tail(c))
+  add_indices(p, i, j)
+end
+
+@inline function tdot(p::Ptr{T}, a::Tuple{A1,A2,Vararg}, b::Tuple{B1,B2,Vararg}, c::Tuple{C1}) where {T,A1,A2,B1,B2,C1}
+  i = lazymul(  first(a), first(b), first(c))
+  p, j = tdot(p, tail(a),  tail(b))
+  add_indices(p, i, j)
 end
 
