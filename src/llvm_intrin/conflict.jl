1859668f42b240e02b41b28ef3f14e8d9c693b87
Chris Elrod <elrodc@gmail.com>
2022-07-22 16:25:50 -0400

    add vpconflict

---
+++ src/llvm_intrin/conflict.jl
@@ -0,0 +1,25 @@
+
+function conflictquote(W::Int=16, bits::Int = 32)
+  @assert bits == 32 || bits == 64
+  s = bits == 32 ? 'd' : 'q'
+  typ = "i$(bits)"
+  vtyp = "<$W x $(typ)>";
+  op = "@llvm.x86.avx512.conflict.$s.$(bits*W)"
+  decl = "declare <$W x $(typ)> $op(<$W x $(typ)>)"
+  instrs = "%res = call <$W x $(typ)> $op(<$W x $(typ)> %0)\n ret <$W x $(typ)> %res"
+  T = Symbol(:UInt, bits)
+  llvmcall_expr(
+    decl,
+    instrs,
+    :(_Vec{$W,$T}),
+    :(Tuple{_Vec{$W,$T}}),
+    vtyp,
+    [vtyp],
+    [:(data(v))],
+  )
+end
+
+@generated function vpconflict(v::Vec{W,T}) where {W,T}
+  conflictquote(W, 8sizeof(T))
+end
+
